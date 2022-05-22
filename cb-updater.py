a21df52459acb633bc7f7a0e7dbf3b33ee7a22e1
Aetopia <kirbymario22@gmail.com>
2022-05-22 14:12:44 +0530

    Better Release Detection + Updated Installation Method

--- cb-updater.py
+++ cb-updater.py
@@ -1,43 +1,92 @@
 from urllib.request import urlopen, urlretrieve
 from tempfile import gettempdir
-from shutil import unpack_archive, copytree
+from shutil import unpack_archive, copytree, rmtree
 from platform import system
 from glob import glob
 from json import load, dumps
 from sys import exit
 from pathlib import Path
 from os import path
-from getpass import getpass
+from ast import literal_eval
+from sys import exit
 
 url = 'https://api.github.com/repos/TellinqBreaker/CheatBreaker/releases'
-file = load(urlopen(url))[0]['assets'][0]['browser_download_url']
+profile_not_exist = True
 temp = (f'{gettempdir()}/CheatBreaker.zip', f'{gettempdir()}/CheatBreaker')
-if '1.7.10' not in file:
-    exit()
 if system() == 'Windows': mc_dir = f'{Path().home()}/AppData/Roaming/.minecraft'
 else: mc_dir = f'{Path().home()}/.minecraft'  
+json = load(urlopen(url))
+
+print('Releases:')
+for index, entry in enumerate(json):
+    if entry['prerelease'] == True: type = 'Pre-Release'
+    else: type = 'Release'
+    print(f"{str(index+1).center(len(str(len(json)))).lstrip()} | {entry['name']} > {type}")
+
+while True:
+    try: entry = json[literal_eval(input('Select > '))-1]; break
+    except KeyboardInterrupt: exit()
+
+print(f"\nRelease Info > {entry['name']}:\n{entry['body']}".rstrip('\n'))
+if len(entry['assets']) not in (0,1):
+    print('\nFiles:')
+    for index, asset in enumerate(entry['assets']):
+        print(f"{str(index+1).center(len(str(len(entry['assets'])))).lstrip()} | {asset['name']}")
+    while True:
+        try:
+            option = literal_eval(input('\nSelect > ').lower().strip())
+            print(entry['assets'][option-1]['browser_download_url'])
+            break
+        except KeyboardInterrupt: exit() 
+else: 
+    while True:
+        option = input('\nInstall? (Y/N) > ').lower().strip()
+        if option in ('yes','y',''): file = entry['assets'][0]['browser_download_url']; break
+        elif option in ('n','no'): exit() 
 
 # Download and Install CheatBreaker 1.7.10
-print('Downloading the latest CheatBreaker 2 Release...')
+print(f'\nDownloading ({path.split(file)[1]})...')
 urlretrieve(file, temp[0])
-print('Unpacking Release...')
+print('Extracting...')
+if path.exists(temp[1]) and path.isdir(temp[1]): rmtree(temp[1]) 
 unpack_archive(temp[0], temp[1])
-print("Copying CheatBreaker 2's files to .minecraft's versions folder...")
-copytree(path.dirname(tuple(Path(glob(temp[1])[0]).rglob('*'))[0]), f'{mc_dir}/versions', dirs_exist_ok=True)
+folders = glob(f'{temp[1]}/*')
+if len(folders) != 1:
+    print('\nFolders:')
+    for index, folder in enumerate(folders):
+        if path.isdir(folder):
+            print(f'{str(index+1).center(len(str(len(folders)))).lstrip()} | {path.split(folder)[1]}')
+    while True:
+        try:
+            option = input('Select > ').lower().strip()
+            folder = folders[literal_eval(option)-1]
+            if path.isdir(folder) is False: continue
+            with open(f'{glob(f"{folder}/*.json")[0]}') as version_json:
+                version = load(version_json)['id']
+                break
+        except KeyboardInterrupt: exit()   
+    print()    
+else:
+    folder = folders[0]
+    for version_json_file in Path(temp[1]).rglob('*.json'):
+        if path.isfile(version_json_file):
+            with open(version_json_file) as version_json:
+                version = load(version_json)['id']
+                break
+print(f"Copying ({version}) to (.minecraft)...")
+copytree(folder, f'{mc_dir}/versions', dirs_exist_ok=True)
 
 # Add a Cheatbreaker 1.7.10 Profile if it doesn't exist.
 with open(f'{mc_dir}/launcher_profiles.json') as launcher_profiles:
     launcher_profiles = load(launcher_profiles)
-    profile_not_exist = False    
+
 for profile in tuple(launcher_profiles['profiles'].values()):
-    if 'cheatbreaker-1.7.10' != profile['lastVersionId'].lower(): profile_not_exist = True
-    else: profile_not_exist = False;break
+    if profile['lastVersionId'].lower() == version.lower(): profile_not_exist = False
 if profile_not_exist:
-    print('Making a Profile for Cheatbreaker 2...')
-    launcher_profiles['profiles']['CheatBreaker 1.7.10'] = {'created': '', 'icon': 'Furnace', 'lastUsed': '', 'lastVersionId': 'CheatBreaker-1.7.10', 'name': 'CheatBreaker 1.7.10', 'type': ''}
+    print(f'Making a Profile for ({version})...')
+    launcher_profiles['profiles'][version] = {'created': '', 'icon': 'Furnace', 'lastUsed': '', 'lastVersionId': version, 'name': version, 'type': ''}
     with open(f'{mc_dir}/launcher_profiles.json', 'w') as file:
-        file.write(dumps(launcher_profiles, indent=4))   
-print('Installation finished.')
-getpass('\nPress Enter to exit.')
+        file.write(dumps(launcher_profiles, indent=4))       
+print('Installation Finished.')
 
 
